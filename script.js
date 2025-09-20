8490b9c846bffa479a20b041a59b4b416bd16ede
renonhash-ctrl <renonhash@gmail.com>
2025-09-20 21:51:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `I know na hindi mo kayang lagpasan mga message ko sayo, so here's my gift to you baby.\nI want you to always remember that you are truly loved. not just in words, but in actions, in presence, and in every little way i can show you.\n\nI will be here with you through every season of your life, whether it’s bright days full of laughter or quiet nights when things feel heavy.\n\nI’ll keep supporting you no matter what you face,\n\nAnyway, I hope you're doing okay. i care about you a lot so stay safe everyday mylove.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
